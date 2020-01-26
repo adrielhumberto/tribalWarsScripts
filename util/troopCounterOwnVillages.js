117a4dbab76daf8e83682f50085d7dbd1bd68d33
FunnyPocketBook <dang.yingkai@gmail.com>
2020-01-26 15:45:42 +0100

    Add coordinates of target village to return object Add function to create window with backtimes Adjusted ESLint to TW global variables

--- util/troopCounterOwnVillages.js
+++ util/troopCounterOwnVillages.js
@@ -83,7 +83,7 @@ villageListHttp.send();
  * @param {String} continent continent of units ("K[0-99]", "all")
  * @returns {Integer} Sum of unit in the village that are in location
  */
-function sumUnits (unit, location, continent) {
+function sumUnits(unit, location, continent) {
     let sum = 0;
     Object.keys(villageList).forEach(function (key) {
         let village = villageList[key];
@@ -97,6 +97,7 @@ function sumUnits (unit, location, continent) {
 sumUnits("spear", "inVillage", "all"); // Sum of all spear in village from all continents
 sumUnits("axe", "own", "K54"); // Sum of all spear that belong to you and are in the village in continent 54
 
+
 /**
  * Sums units in a radius
  * @param {String} unit name of unit
@@ -106,7 +107,7 @@ sumUnits("axe", "own", "K54"); // Sum of all spear that belong to you and are in
  * @param {Integer} radius radius in fields of circle
  * @returns {Integer} Sum of unit in the village that are in location within the radius
  */
-function sumUnitsRadius (unit, location, centerX, centerY, radius) {
+function sumUnitsRadius(unit, location, centerX, centerY, radius) {
     let sum = 0;
     Object.keys(villageList).forEach(function (key) {
         const village = villageList[key];
@@ -122,15 +123,17 @@ function sumUnitsRadius (unit, location, centerX, centerY, radius) {
 }
 sumUnitsRadius("axe", "total", 454, 562, 5); // Sum of total axe count in a radius of 5 fields from 454|562
 
+
 /**
  * Gets all villages that can send a backtime to target
  * @param {Date} targetDate Date of when enemy troops return to the village
  * @param {Integer} targetX x-coordinate of enemy village
  * @param {Integer} targetY y-coordinate of enemy village
+ * @param {String[]} units Array with units to include. ["all"] to include every unit
  * @returns {Object} Returns object with villages as keys that can send backtimes. Each village has units as keys that are fast enough for a backtime
  */
-function backtimeVillages(targetDate, targetX, targetY) {
-    let backtimeVillages = {};
+function backtimeVillages(targetDate, targetX, targetY, units) {
+    let backtimeVillages = { "targetVillage": targetX + "|" + targetY };
     // Goes through every village in villageList and calculates the distance from village to targetVillage
     Object.keys(villageList).forEach(function (villageId) {
         const village = villageList[villageId];
@@ -142,7 +145,7 @@ function backtimeVillages(targetDate, targetX, targetY) {
             const runtimeMs = unitSpeed[unit] * distance * 60 * 1000; // Time for unit to travel in milliseconds
             const sendBacktimeDate = new Date(targetDate - runtimeMs); // Calculates when unit has to be sent in order to arrive on targetDate
             // Checks if sendBacktimeDate is in the future (meaning sending backtime is possible) and the amount of unit that should be sent is in the village, ready to be sent
-            if (sendBacktimeDate > new Date() && village.units.own[unit] > 0) {
+            if ((units.includes("all") || units.includes(unit)) && sendBacktimeDate > new Date() && village.units.own[unit] > 0) {
                 if (!backtimeVillages[villageId]) {
                     backtimeVillages[villageId] = {
                         "coords": village.coords,
@@ -154,7 +157,7 @@ function backtimeVillages(targetDate, targetX, targetY) {
                 }
                 backtimeVillages[villageId][unit] = {
                     "sendDate": sendBacktimeDate,
-                    "amount": village.units.own[unit] 
+                    "amount": village.units.own[unit]
                 };
             }
         });
@@ -167,4 +170,86 @@ function backtimeVillages(targetDate, targetX, targetY) {
 
 const dateNow = new Date();
 const arrivalDate = new Date(dateNow.setHours(dateNow.getHours() + 4)); // Enemy troops are returning 4 hours from now
-backtimeVillages(arrivalDate, 447, 533);
+let backUnits = backtimeVillages(arrivalDate, 447, 533, ["axe", "light", "snob"]);
+
+
+/**
+ * Returns a sorted array by date for each unit in the backtimeObject
+ * @param {Object} backtimeObject Object returned by backtimeVillages
+ * @returns Array of units by ascending date
+ */
+function sortUnitByDate(backtimeObject) {
+    let units = [];
+    // Goes through every village and entry in that village object. Creates a new array element if entry is a unit
+    Object.values(backtimeObject).forEach(function (village) {
+        Object.keys(village).forEach(function (unit) {
+            if (game_data.units.includes(unit)) {
+                let unitObj = village[unit];
+                let obj = {
+                    "villageId": village.id,
+                    "villageName": village.name,
+                    "villageCoords": village.coords,
+                    "sendDate": unitObj.sendDate,
+                    "unitAmount": unitObj.amount,
+                    "unitName": unit
+                };
+                units.push(obj);
+            }
+        });
+    });
+    // Sorts array ascending by date
+    units.sort(function (a, b) {
+        return new Date(a.sendDate) - new Date(b.sendDate);
+    });
+    return units;
+}
+let sortedUnits = sortUnitByDate(backUnits);
+
+
+/**
+ * Creates a new popup window with a table of backtimes
+ * @param {Object[]} sortedUnits Array of objects returned by sortUnitByDate
+ */
+function parseBacktime(sortedUnits) {
+    let main_div = "<div id='bb_main_div' class='popup_stlye' style='display: block; top: 100px; left: 700px; z-index: 99999; position: fixed; border-radius: 8px; border: 2px #804000 solid; background-color: #F1EBDD'></div>";
+    let close_button = "<div class='popup_menu'><a href='Javascript:void(0);' id='a_close'>Close</a></div>";
+    let output_text = "<div class='popup_content' style='padding 8px'><h3 align='center'>Backtime</h3></br><table><thead><tr><th>Village</th><th>Unit</th><th>Amount</th><th>Send Date</th></tr></thead><tbody>";
+    const rowColor = ["row_a", "row_b"];
+    let counter = 0;
+    $("body").append(main_div);
+    $("#bb_main_div").draggable();
+    $("#bb_main_div").append(close_button);
+    $("#a_close").click(function () {
+        $("#bb_main_div").remove();
+    });
+    sortedUnits.forEach(function (entry) {
+        output_text += `
+        <tr class="${rowColor[counter++ % 2]}">
+            <td><a href="/game.php?village=${entry.villageId}&screen=overview">${entry.villageName}</a></td>
+            <td><img src="${image_base}unit/unit_${entry.unitName}.png" title="${entry.unitName}" alt="" class=""></td>
+            <td>${entry.unitAmount}</td>
+            <td>${formatDateTW(entry.sendDate)}</td>
+        </tr>`;
+    });
+    output_text += "</tbody></div>";
+    $("#bb_main_div").append(output_text);
+}
+parseBacktime(sortedUnits);
+
+/**
+ * Formats date in format of game locale
+ * @param {Date} date Date to be formatted
+ */
+function formatDateTW(date) {
+    const options = {
+        "weekday": "short",
+        "year": "2-digit",
+        "month": "2-digit",
+        "day": "2-digit",
+        "hour": "2-digit",
+        "minute": "2-digit",
+        "second": "2-digit",
+        "hour12": false
+    };
+    return date.toLocaleDateString(game_data.locale.replace("_", "-"), options);
+}
