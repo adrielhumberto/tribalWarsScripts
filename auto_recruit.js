5c299eda5e60d77fdc3e1d20b65da7e1f736a419
FunnyPocketBook <dang.yingkai@gmail.com>
2020-01-27 00:35:21 +0100

    Start over on auto_recruit

--- auto_recruit.js
+++ auto_recruit.js
@@ -1,162 +1,63 @@
-const hostname = window.location.hostname;
-const villageID = game_data.village.id;
-const unitInfoUrl = "https://" + hostname + "/interface.php?func=get_unit_info";
-let archer, knight, militia;
-let village;
-let maxUnits = new Units();
-maxUnits.spear = 1500;
-maxUnits.sword = 1500;
-maxUnits.axe = 1000;
-maxUnits.archer = 1500;
-maxUnits.spy = 500;
-maxUnits.light = 500;
-maxUnits.marcher = 0;
-maxUnits.heavy = 400;
-maxUnits.ram = 20;
-maxUnits.catapult = 10;
-main();
-
-function main() {
-    if (localStorage.autoRecruit === undefined) {
-        localStorage.autoRecruit = "{\"villageList\": []}";
-    }
-    getConfigUnits();
-    getTotalUnits(villageID);
-}
 /**
-* Store village information
-*/
-class VillageObj {
-    constructor(units, maxUnits, resources) {
-        this.id = villageID;
-        this.units = {
-            spear: units.spear,
-            sword: units.sword,
-            axe: units.axe,
-            archer: units.archer,
-            spy: units.spy,
-            light: units.light,
-            marcher: units.marcher,
-            heavy: units.heavy,
-            ram: units.ram,
-            catapult: units.catapult
-        };
-        this.maxUnits = maxUnits;
-        this.resources = {
-            wood: resources[0],
-            stone: resources[1],
-            iron: resources[2]
-        }
-    }
-}
-
-class Units {
-    constructor() {
-        this.archer = 0;
-        this.marcher = 0;
-        this.spear = 0;
-        this.sword = 0;
-        this.axe = 0;
-        this.spy = 0;
-        this.light = 0;
-        this.heavy = 0;
-        this.ram = 0;
-        this.catapult = 0;
-    }
-}
-
-/**
- * Get total amount of units that belong to the village
+ * Get total amount of units that belong to the village and current resources
+ * @param {{}}} villageList Object with villages as entry
  */
-function getTotalUnits(villageID) {
-    let villageList = JSON.parse(localStorage.autoRecruit);
-    $.get("https://" + hostname + "/game.php?village=" + villageID +"&screen=train", function(data) {
-        let doc = new DOMParser().parseFromString(data, "text/html");
-        const unitTypeLength = doc.querySelector("#train_form > table:nth-child(1)").rows.length;
-        let units = new Units();
-        for (let i = 2; i < unitTypeLength; i++) {
-            let unitName = doc.querySelector("#train_form > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(" + i + ") > td:nth-child(1) > a:nth-child(1)").getAttribute("data-unit");
-            units[unitName] = doc.querySelector("#train_form > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(" + i + ") > td:nth-child(3)").innerText.split("/")[1];
+function getVillageInfo(villageList) {
+    $.get(`${document.location.origin}/game.php?village=${game_data.village.id}&screen=train&mode=mass&page=-1`, function (data) {
+        const doc = new DOMParser().parseFromString(data, "text/html");
+        const unitRecruitTable = doc.getElementById("mass_train_table").rows;
+        for (let i = 1; i < unitRecruitTable.length; i++) {
+            const id = parseInt(unitRecruitTable[i].cells[0].getElementsByTagName("a")[0].getAttribute("href").match(/village=[0-9]*/)[0].replace("village=", ""));
+            const name = unitRecruitTable[i].cells[0].getElementsByTagName("a")[0].textContent;
+            const wood = parseInt(unitRecruitTable[i].cells[1].querySelector(".res.wood").textContent.replace(".", ""));
+            const stone = parseInt(unitRecruitTable[i].cells[1].querySelector(".res.stone").textContent.replace(".", ""));
+            const iron = parseInt(unitRecruitTable[i].cells[1].querySelector(".res.iron").textContent.replace(".", ""));
+            const farm = unitRecruitTable[i].cells[2].textContent.split("/");
+            const farmSpace = parseInt(farm[1]) - parseInt(farm[0]);
+            let units = {};
+            for (let j = 3; j < unitRecruitTable[i].cells.length; j++) {              
+                let src = unitRecruitTable[0].cells[j].firstElementChild.getAttribute("src");
+                let unitName = src.substring(src.lastIndexOf("/") + 1).replace("unit_", "").replace("(1)", "").split(".")[0];
+                let unitAmount = parseInt(unitRecruitTable[i].cells[j].firstElementChild.textContent.trim());
+                units[unitName] = unitAmount;
+            }
+            if (!villageList[id]) {
+                villageList[id] = {};
+            }
+            villageList[id].villageInfo = {
+                "id": id,
+                "name": name,
+                "wood": wood,
+                "stone": stone,
+                "iron": iron,
+                "farmSpace": farmSpace
+            };
+            villageList[id].units = units;
         }
-        village = new VillageObj(units, [document.getElementById("wood").innerText, document.getElementById("stone").innerText, document.getElementById("iron").innerText]);
-        villageList.villageList.push(village);
         localStorage.autoRecruit = JSON.stringify(villageList);
-    })
-}
-
-/**
- * Check the available units (archer, marcher, paladin, militia)
- */
-function getConfigUnits() {
-    $.get(unitInfoUrl, function(data) {
-        let r = xmlToJson(data)
-        archer = r.config.archer !== undefined;
-        knight = r.config.knight !== undefined;
-        militia = r.config.militia !== undefined;
-    })
+    });
 }
 
 /**
  * Trains unitAmount of unit
- * @param {String}  unit        Name of unit
- * @param {Integer} unitAmount  Amount of unit to train
+ * @param {string}  unit        Name of unit
+ * @param {number} unitAmount  Amount of unit to train
  */
 function recruitUnit(unit, unitAmount) {
-    let data = {};
+    let data = {h: game_data.csrf};
     data["units[" + unit + "]"] = unitAmount;
-    let url = "game.php?village=" + game_data.village.id + "&screen=train&ajaxaction=train&h=" + game_data.csrf + "&client_time=" + Math.floor(Timing.getCurrentServerTime()/1000);
-    $.post(url, data);  
+    TribalWars.post("train", {ajaxaction: "train", mode: "train"}, data, function(r) {
+        if (r.success) {
+            console.log(`Trained ${unitAmount} of ${unit}`);
+            if (document.getElementsByClassName("current_prod_wrapper")[0]) {
+                document.getElementsByClassName("current_prod_wrapper")[0].innerHTML = r.current_order;
+            }
+        }
+    });
 }
 
-function getCurrentRecruit(villageID) {
-    
-}
-
-
-
-
-
-//#region Utility
 
 /**
- * Convert XML to JSON, used for world configs
- * @param {XML} xml 
+ * - Get units in training -> Where? Don't send too many requests
+ * - Only get units in current training for one village
  */
-function xmlToJson(xml) {
-	// Create the return object
-	let obj = {};
-
-	if (xml.nodeType == 1) { // element
-		// do attributes
-		if (xml.attributes.length > 0) {
-		obj["@attributes"] = {};
-			for (let j = 0; j < xml.attributes.length; j++) {
-				let attribute = xml.attributes.item(j);
-				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-			}
-		}
-	} else if (xml.nodeType == 3) { // text
-		obj = xml.nodeValue;
-	}
-
-	// do children
-	if (xml.hasChildNodes()) {
-		for(let i = 0; i < xml.childNodes.length; i++) {
-			let item = xml.childNodes.item(i);
-			let nodeName = item.nodeName;
-			if (typeof(obj[nodeName]) == "undefined") {
-				obj[nodeName] = xmlToJson(item);
-			} else {
-				if (typeof(obj[nodeName].push) == "undefined") {
-					let old = obj[nodeName];
-					obj[nodeName] = [];
-					obj[nodeName].push(old);
-				}
-				obj[nodeName].push(xmlToJson(item));
-			}
-		}
-	}
-	return obj;
-};
-
-//#endregion Utility
