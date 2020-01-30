ad0d331cc795702a2eb30ce7529f0aa1b489cb28
FunnyPocketBook <dang.yingkai@gmail.com>
2020-01-30 02:31:10 +0100

    Notify user which units have been recruited or if there was a problem Add locale Create table for auto recruit setup

--- auto_recruit.js
+++ auto_recruit.js
@@ -1,20 +1,57 @@
+const locales = {
+    "en": {
+        "train": {
+            "errorTrainUnit": "A problem occurred recruiting some units.",
+            "successTrainUnit": "All units have been recruited successfully."
+        },
+        "fields": {
+            "recruitUnits": "Recruit",
+            "selectGroup": "Select Group",
+            "village": "Village",
+            "units": "Units",
+            "puffer": "Puffer"
+        },
+        "util": {
+            "lookAtConsole": "Please open the browser console for more information."
+        }
+    },
+    "de": {
+        "train": {
+            "errorTrainUnit": "Es gab ein Problem bei der Rekrutierung einiger Einheiten.",
+            "successTrainUnit": "Alle Einheiten wurden erfolgreich rekrutiert."
+        },
+        "fields": {
+            "recruitUnits": "Rekrutieren",
+            "selectGroup": "Gruppe Auswählen",
+            "village": "Dorf",
+            "units": "Einheiten",
+            "puffer": "Puffer"
+        },
+        "util": {
+            "lookAtConsole": "Bitte öffne die Browser-Konsole für mehr Informationen."
+        }
+    }
+};
+const locale = locales[game_data.locale.split("_")[0]];
+
 /**
  * Get total amount of units that belong to the village and current resources
  * @param {{}}} villageList Object with villages as entry
  */
 function getVillageInfo(villageList) {
-    $.get(`${document.location.origin}/game.php?village=${game_data.village.id}&screen=train&mode=mass&page=-1`, function (data) {
+    return $.get(`${document.location.origin}/game.php?village=${game_data.village.id}&screen=train&mode=mass&page=-1`, function (data) {
         const doc = new DOMParser().parseFromString(data, "text/html");
         const unitRecruitTable = doc.getElementById("mass_train_table").rows;
         for (let i = 1; i < unitRecruitTable.length; i++) {
             const id = parseInt(unitRecruitTable[i].cells[0].getElementsByTagName("a")[0].getAttribute("href").match(/village=[0-9]*/)[0].replace("village=", ""));
             const name = unitRecruitTable[i].cells[0].getElementsByTagName("a")[0].textContent;
-            const wood = parseInt(unitRecruitTable[i].cells[1].querySelector(".res.wood").textContent.replace(".", ""));
-            const stone = parseInt(unitRecruitTable[i].cells[1].querySelector(".res.stone").textContent.replace(".", ""));
-            const iron = parseInt(unitRecruitTable[i].cells[1].querySelector(".res.iron").textContent.replace(".", ""));
+            const wood = parseInt(unitRecruitTable[i].cells[1].querySelector(".wood").textContent.replace(".", ""));
+            const stone = parseInt(unitRecruitTable[i].cells[1].querySelector(".stone").textContent.replace(".", ""));
+            const iron = parseInt(unitRecruitTable[i].cells[1].querySelector(".iron").textContent.replace(".", ""));
             const farm = unitRecruitTable[i].cells[2].textContent.split("/");
             const farmSpace = parseInt(farm[1]) - parseInt(farm[0]);
             let units = {};
+            // Gets unit info
             for (let j = 3; j < unitRecruitTable[i].cells.length; j++) {
                 let src = unitRecruitTable[0].cells[j].firstElementChild.getAttribute("src");
                 let unitName = src.substring(src.lastIndexOf("/") + 1).replace("unit_", "").replace("(1)", "").split(".")[0];
@@ -49,6 +86,10 @@ function getVillageInfo(villageList) {
             };
             villageList[id].units = units;
         }
+        
+        TribalWars.get("groups", { ajax: "load_groups" }, function (response) {
+            villageList.groups = response.result;
+        });
         localStorage.autoRecruit = JSON.stringify(villageList);
     });
 }
@@ -59,7 +100,7 @@ function getVillageInfo(villageList) {
  */
 function recruitUnits(recruitUnits) {
     recruitUnits.h = game_data.csrf;
-    $.ajax({
+    return $.ajax({
         url: `${document.location.origin}/game.php?village=${game_data.village.id}&screen=train&mode=success&action=train_mass&page=-1`,
         type: "post",
         data: recruitUnits,
@@ -78,7 +119,110 @@ function recruitUnits(recruitUnits) {
             };
             return xhr;
         },
-    }).done(function () { // TODO: Check which units have been recruited and notify user
-        console.log("done");
-    });
+    })
+        // Nofity user what has been recruited and if there were any problems
+        .done(function (r) {
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(r, "text/html");
+            let errors = doc.getElementsByClassName("error_box");
+            if (errors.length !== 0) {
+                let villageName = doc.getElementById("menu_row2_village").textContent.trim();
+                let villageCoord = doc.getElementById("menu_row2_village").nextElementSibling.textContent.trim();
+                UI.ErrorMessage(locale.train.errorTrainUnit + ".\n " + locale.util.lookAtConsole);
+                console.log(locale.train.errorTrainUnit);
+                for (let error of errors) {
+                    console.log(`${villageName} ${villageCoord} ${error.textContent.trim()}`);
+                }
+            } else {
+                console.log(locale.train.successTrainUnit);
+                const table = doc.getElementsByClassName("vis")[0].rows;
+                for (let i = 1; i < table.length; i++) {
+                    const villageName = table[i].cells[0].textContent.trim();
+                    const unitsNodes = table[i].cells[1].childNodes;
+                    let units = "";
+                    for (let j = 1; j < unitsNodes.length; j += 2) {
+                        var unitName = unitsNodes[j].getAttribute("title");
+                        units += unitName + ": " + parseInt(unitsNodes[j + 1].textContent.trim()) + ", ";
+                    }
+                    console.log(villageName + ": " + units.substring(0, units.length - 2));
+                }
+            }
+        });
 }
+
+function createTable(villageList) {
+    const rowColor = ["row_a", "row_b"];
+    let counter = 0;
+    let table = `
+    <table class="vis" width="100%">
+        <thead>
+            <tr>
+                <th>${locale.fields.village}</th>
+                <th>${locale.fields.puffer}</th>`;
+    for (let unit of game_data.units) {
+        if (!unit.includes("snob") && !unit.includes("militia") && !unit.includes("knight")) {
+            table += `
+            <th style="text-align:center" width="35">
+                <a href="#" class="unit_link" data-unit="${unit}">
+                    <img src="${image_base}unit/unit_${unit}.png" title="${unit}" alt="" class="">
+                </a>
+            </th>`;
+        }
+    }
+    table += "</thead>";
+    Object.keys(villageList).forEach(function (villageId) {
+        let village = villageList[villageId];
+        let villageName = village.villageInfo.name;
+        let units = village.units;
+        table += `
+        <tr class="${rowColor[counter++ % 2]}">
+            <td class="vis_item">
+                <input class="am_troops_edit" type="checkbox" name="edit[]" value="${villageId}" style="float: left;">
+                <span>
+                    <span class="village_anchor" data-player="${game_data.player.id}" data-id="${villageId}">
+                        <a href="${game_data.link_base_pure}info_village&id=${villageId}">${villageName}</a>
+                    </span>
+                </span>
+            </td>
+            <td>
+                <span class="icon header wood"></span>
+                 
+                <span data-field="buffer_wood">${village.villageInfo.wood}</span>
+                 
+                <span class="icon header stone"></span>
+                 
+                <span data-field="buffer_stone">${village.villageInfo.stone}</span>
+                 
+                <span class="icon header iron"></span>
+                 
+                <span data-field="buffer_iron">${village.villageInfo.iron}</span>
+                 
+                <span class="icon header population"></span>
+                 
+                <span data-field="buffer_pop">${village.villageInfo.farmSpace}</span>
+            </td>`;
+        for (let unitName of game_data.units) {
+            if (!unitName.includes("snob") && !unitName.includes("militia") && !unitName.includes("knight")) {
+                table += `
+                <td>
+                    <span>${units[unitName].currentAmount}</span>
+                    <br>
+                    <span">${units[unitName].inTraining}</span>
+                    <br>
+                    <span data-field="${unitName}">${units[unitName].trainableAmount}</span>
+                </td>`;
+            }
+        }
+    });
+    table += "</tbody></table>";
+    document.getElementById("content_value").innerHTML = table;
+    VillageContext.init($("#content_value"));
+}
+
+async function createTableAsync(villageList) {
+    await getVillageInfo(villageList);
+    createTable(villageList);
+}
+
+let villageList = {};
+createTableAsync(villageList);
